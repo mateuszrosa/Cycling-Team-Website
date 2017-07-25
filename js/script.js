18308f150a03ad76d750f88fc74022a594868290
mateuszrosa <mateusz.rosa@gmail.com>
2017-07-25 16:29:42 +0200

    hamburger and mobile menu

--- js/script.js
+++ js/script.js
@@ -89,5 +89,15 @@ let news = document.querySelectorAll('#second .text');
     }
   })
 
+  let hamburger = document.querySelector('.hamburger');
+  let mobile = document.querySelector('#mobilemenu');
+
+    hamburger.addEventListener('click', function() {
+      if(mobile.style.display == 'block') {
+        mobile.style.display = 'none';
+      } else {
+        mobile.style.display = 'block';
+      }
+    })
 
 })
